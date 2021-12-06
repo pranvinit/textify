c2d05a2b6f344a2b9b7c4bf3af3f3d27a370445b
Pranav <pranvofficial@gmail.com>
2021-12-06 09:25:01 +0530

    added static files and templates

--- textifyapi/views.py
+++ textifyapi/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Hello this is my index page")
+    return render(request, 'textifyapi/index.html')
